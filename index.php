02c7b0263267c07eccc6467228bc84ef6f21ae2c
EvanderVegt <e.vandervegt03@gmail.com>
2017-10-26 13:57:31 +0200

    start Lesrooster met array's

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+$les = array("Wiskunde","Natuurkunde","Biologie","Scheidkunde");
+$naam = array("Thomas","Bas","Erik");
+$weekDag = array("Maandag","Dinsdag","Woensdag","Donderdag","Vrijdag");
+
+
+
+
+
+
+
+
+
+
+?>
